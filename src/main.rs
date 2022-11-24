21bc3330baf71272161ebf5f09f5fb4392497638
kirinokirino <kirinokirino@lunariko.com>
2022-11-24 09:06:20 +0300

    Parse the text into highlighting events.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use macroquad::prelude::{
     clear_background, color_u8, draw_text_ex, get_char_pressed, load_ttf_font, next_frame, Color,
     TextParams,
 };
+use tree_sitter_highlight::{HighlightConfiguration, HighlightEvent, Highlighter};
 use walkdir::{DirEntry, Error, WalkDir};
 
 use std::{fs::read_to_string, path::PathBuf};
@@ -10,6 +11,7 @@ use std::{fs::read_to_string, path::PathBuf};
 async fn main() {
     let font = load_ttf_font("./assets/FiraCodeMono.ttf").await.unwrap();
     let mut game = Game::new(Font::new(font));
+
     while game.is_running {
         game.update();
         game.draw();
@@ -20,9 +22,12 @@ async fn main() {
 struct Game {
     font: Font,
     text_to_type: String,
+    highlights_orig: Vec<HighlightEvent>,
     text_typed: String,
     is_running: bool,
     possible_paths: Vec<PathBuf>,
+    highlighter: Highlighter,
+    highlighter_config: HighlightConfiguration,
 }
 
 impl Game {
@@ -36,13 +41,59 @@ impl Game {
             .unwrap();
         let text = read_to_string(path).unwrap_or("Please press TAB!".to_owned());
 
+        let highlight_names = &[
+            "attribute",
+            "constant",
+            "function.builtin",
+            "function",
+            "keyword",
+            "operator",
+            "property",
+            "punctuation",
+            "punctuation.bracket",
+            "punctuation.delimiter",
+            "string",
+            "string.special",
+            "tag",
+            "type",
+            "type.builtin",
+            "variable",
+            "variable.builtin",
+            "variable.parameter",
+        ];
+
+        let highlighter = Highlighter::new();
+
+        let mut rust_config = HighlightConfiguration::new(
+            tree_sitter_rust::language(),
+            tree_sitter_rust::HIGHLIGHT_QUERY,
+            "",
+            "",
+        )
+        .unwrap();
+
+        rust_config.configure(highlight_names);
+
         Self {
             text_to_type: text,
+            highlights_orig: Vec::new(),
             text_typed: String::new(),
             font,
             is_running: true,
             possible_paths,
+            highlighter,
+            highlighter_config: rust_config,
         }
+    }
+
+    pub fn update_highlighting(&mut self) {
+        let text = &self.text_to_type;
+        let highlights = self
+            .highlighter
+            .highlight(&self.highlighter_config, text.as_bytes(), None, |_| None)
+            .unwrap();
+
+        self.highlights_orig = highlights.map(|event| event.unwrap()).collect();
     }
 
     pub fn update(&mut self) {
@@ -77,6 +128,7 @@ impl Game {
         }
         self.text_to_type = text.unwrap();
         self.text_typed = String::new();
+        self.update_highlighting();
     }
 
     pub fn draw(&self) {
@@ -86,41 +138,24 @@ impl Game {
         for (line_num, line) in self.text_to_type.lines().enumerate() {
             self.font.draw_line(12.0, line_num, line);
         }
+
+        for event in &self.highlights_orig{
+            match event {
+                HighlightEvent::Source { start, end } => {
+                    todo!();
+                }
+                HighlightEvent::HighlightStart(s) => {
+                    todo!();
+                }
+                _ => (),
+            }
+        }
+
         for (line_num, line) in self.text_typed.lines().enumerate() {
             self.font
                 .draw(12.0, line_num, line, color_u8![255, 255, 255, 255]);
         }
     }
-}
-
-fn check_path(e: Result<DirEntry, Error>) -> Option<DirEntry> {
-    if e.is_err() {
-        return None;
-    }
-    let e = unsafe { e.unwrap_unchecked() };
-    if is_hidden(&e) {
-        return None;
-    }
-    if is_rust_file(&e) {
-        return Some(e);
-    }
-    None
-}
-
-fn is_hidden(entry: &DirEntry) -> bool {
-    entry
-        .file_name()
-        .to_str()
-        .map(|s| s.starts_with("."))
-        .unwrap_or(false)
-}
-
-fn is_rust_file(entry: &DirEntry) -> bool {
-    entry
-        .file_name()
-        .to_str()
-        .map(|s| s.ends_with(".rs"))
-        .unwrap_or(false)
 }
 
 struct Font {
@@ -155,7 +190,7 @@ impl Font {
             &line,
             x,
             (self.font_size as f32 * 1.3).floor() * (1 + line_num) as f32,
-            self.font,
+            font,
         );
     }
 
@@ -167,4 +202,34 @@ impl Font {
             self.font,
         );
     }
+}
+
+fn check_path(e: Result<DirEntry, Error>) -> Option<DirEntry> {
+    if e.is_err() {
+        return None;
+    }
+    let e = unsafe { e.unwrap_unchecked() };
+    if is_hidden(&e) {
+        return None;
+    }
+    if is_rust_file(&e) {
+        return Some(e);
+    }
+    None
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+
+fn is_rust_file(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.ends_with(".rs"))
+        .unwrap_or(false)
 }
